4de4eb8a4ab4a2c527f5c5e2aedab8e2aeb9e0ff
Isaac Gerard <IsaacBathSpa@gmail.com>
2024-10-15 09:28:54 +0400

    script.js

---
+++ Class Activities/Task 2 (Grid)/3 Dashboard Interface/script.js
@@ -0,0 +1,22 @@
+// The code runs when the page has fully loaded
+document.addEventListener("DOMContentLoaded", () => {
+
+  // I created a click event listener to the sidebar menu button that shows or hides the sidebar when clicked
+  document.querySelector(".SidebarMenuButton")?.addEventListener("click", () =>
+      document.querySelector(".GridLayoutContainer")?.classList.toggle("active")
+  );
+
+  // I created another click event listener to the user profile icon that toggles the "active" class on the profile dropdown menu when clicked which either shows or hides the profile dropdown menu
+  document.querySelector(".WebsiteHeader .bx-user-circle")?.addEventListener("click", () =>
+      document.querySelector(".User-Profile-Menu-Button-Contents")?.classList.toggle("active")
+  );
+});
+
+// Notification Widget
+function removeNotification(button) {
+  // This gets the parent element of the button, which is the notification div
+  const Notification = button.parentElement;
+  
+  // This removes the notification from the DOM
+  Notification.remove();
+}
